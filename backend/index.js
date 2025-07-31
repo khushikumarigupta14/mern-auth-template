21c12b05f5d579884ad21acb483175fffe111e2f
Khushi Kumari <khushikumarigupta463@gmail.com>
2025-07-31 12:12:38 +0530

    feat: add MongoDB connection setup with Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`The server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`The server is running at http://localhost:${process.env.PORT}`);
 });
